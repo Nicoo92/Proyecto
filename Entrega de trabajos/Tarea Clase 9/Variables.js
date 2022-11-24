92c9d435bd1fa57e8fe7bfc7477e364db09d6434
Nicoo92 <garcianicolas511@gmail.com>
2022-11-23 21:07:10 -0300

    modifique archivos

--- Entrega de trabajos/Tarea Clase 9/Variables.js
+++ Entrega de trabajos/Tarea Clase 9/Variables.js
@@ -1,9 +1,13 @@
+// TAREA CLASE 9
+
+alert(Hello_world);
+
 // VARIABLES
 
-var pais = "España";
-var continente = "Europa";
-var antiguedad = "2021";
-var pais_y_continente = pais + " " + continente;
+let pais = "España";
+let continente = "Europa";
+let antiguedad = "2021";
+let pais_y_continente = pais + " " + continente;
 
 pais = "Mexico";
 continente = "Latinoamerica";

c6fabb1bb930a6d0dad75242685bd0cc02f6add0
Nicoo92 <garcianicolas511@gmail.com>
2022-12-01 18:21:01 -0300

    Actualice tareas

---
+++ Entrega de trabajos/Tarea Clase 10/TareaClase10.js
@@ -0,0 +1,48 @@
+// Primer ejercicio
+// una variable es un contenedor de informacion
+
+let nombre = "Programación" + "Facil.";
+let saludo = "Les doy la bienvenida a mi pagina";
+let comision = "Numero 4";
+let fraseCompleta = saludo + nombre + comision ;
+
+console.log(fraseCompleta);
+
+// Segundo ejercicio
+// una variable es un contenedor de informacion
+
+let frase = "Javascript " + "es un lenguaje" + "esencial para crear" + " sitios web poderosos.";
+
+console.log(frase);
+
+// Tercer ejercicio
+// una variable es un contenedor de informacion
+
+let numeroString1 = "10";
+let numeroString2 = "20";
+let numeroString3 = "30";
+
+let concatenaNumeros = numeroString1 + numeroString2 + numeroString3;
+
+console.log(concatenaNumeros);
+
+// Cuarto ejercicio
+// una variable es un contenedor de informacion
+
+var age = prompt("Cual es tu edad:?");
+console.log(age);
+
+let edad;
+edad = prompt("Introduce tu edad");
+
+if (edad >= 18) {
+    alert("Puedes entrar, eres mayor de edad.");
+} else {
+    alert("No puedes entrar, eres menor de edad");
+}
+
+// Cuarto ejercicio
+// una variable es un contenedor de informacion
+
+let sexo;
+sexo = prompt("Introduce tu sexo");
---
+++ Entrega de trabajos/Tarea Clase 11/TareaClase11.js
@@ -0,0 +1,139 @@
+// Primer ejercicio
+// una variable es un contenedor de informacion
+
+let diaSemana = prompt("¿Que dia es hoy?");
+
+switch (diaSemana) {
+        case "lunes":
+                alert("Hoy es lunes.");
+                break;
+
+        case "martes":
+                alert("Hoy es martes.");
+                break;
+
+        case "miercoles":
+                alert("Hoy es miercoles.");
+                break;
+
+        case "jueves":
+                alert("Hoy es jueves.");                
+                break;
+
+        case "viernes":
+                alert("Hoy es viernes.");
+                break; 
+
+        case "sabado":
+                alert("Hoy es sabado.");
+                break;
+
+        case "domingo":
+                alert("Hoy es domingo.");
+                break;
+
+        default:
+                alert("Escribe el dia de la semana en minusculas.");
+    }
+
+for(i=0;i<=4;i++){
+            document.write("El numero es: " + i + "<br>");
+}
+
+
+// Segundo ejercicio
+// una variable es un contenedor de informacion
+
+let diadeSemana = prompt("¿Que dia es hoy?");
+
+switch (diadeSemana) {
+        case "lunes":
+                alert("Hoy es lunes.");
+                break;
+
+        case "martes":
+                alert("Hoy es martes.");
+                break;
+
+        case "miercoles":
+                alert("Hoy es miercoles.");
+                break;
+
+        case "jueves":
+                alert("Hoy es jueves.");                
+                break;
+
+        case "viernes":
+                alert("Hoy es viernes.");
+                break;
+
+        case "sabado":
+                alert("Hoy es sabado.");
+                break;
+
+        case "domingo":
+                alert("Hoy es domingo.");
+                break;
+            
+        default:
+                alert("Escribe el dia de la semana en minusculas.");
+
+    }
+
+for(i=20;i<=70;i++){
+            document.write("El numero es: " + i + "<br>");
+}
+
+
+// Tercer ejercicio
+// una variable es un contenedor de informacion
+
+let ejercicio ="Lunes";
+
+switch (ejercicio) {
+        case "lunes":
+         console.log("Caminar 5km");
+        break;
+
+        case "martes":
+         console.log("caminar 10km");
+        break;
+
+        case "miercoles":
+         console.log("trotar 5km");
+        break;
+
+        case "jueves":
+         console.log("trotar 10km");                
+        break;
+
+        case "viernes":
+         console.log("flexiones de brazos");
+        break; 
+
+        case "sabado":
+         console.log("abdominales");
+        break;
+
+        case "domingo":
+         console.log("sentadillas");
+        break;
+
+        default:
+          console.log("Descanso");
+    }
+
+for(i=0;i<=4;i++){
+            document.write("El numero es: " + i + "<br>");
+}
+
+
+// Cuarto ejercicio
+// una variable es un contenedor de informacion
+
+let count = 30;
+
+while(count > 0){
+console.log("Hola mundo");
+count = count - 1;
+}
